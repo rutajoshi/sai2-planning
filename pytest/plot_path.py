00b44e90806a2a26b1878247b48484746f4a05cf
Ruta Joshi <rjoshi@berkeley.edu>
2022-03-13 11:40:46 -0700

    Finished plotting in python

---
+++ pytest/plot_path.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_grid_from_file(filename):
+    statespace_lo = [0,0]
+    statespace_hi = []
+    resolution = 0.0
+    with open(filename) as file:
+        for line in file:
+            x, y, resolution = line.strip().split(",")
+            x, y, resolution = float(x), float(y), float(resolution)
+            statespace_hi = [x, y]
+    return [statespace_lo, statespace_hi, resolution]
+
+def get_obstacles_from_file(filename):
+    obstacles = []
+    centerX, centerY, radius = 0, 0, 0
+    with open(filename) as file:
+        for line in file:
+            centerX, centerY, radius = line.strip().split(",")
+            centerX, centerY, radius = float(centerX), float(centerY), float(radius)
+            obstacles.append([centerX, centerY, radius])
+    return obstacles
+
+def get_path_from_file(filename):
+    path = []
+    with open(filename) as file:
+        for line in file:
+            line = line.strip().split(",")
+            line = [float(x) for x in line]
+            path.append(line)
+    path = np.array(path)
+    return path
+
+def plot_everything(grid, obstacles, path):
+    statespace_lo, statespace_hi, resolution = grid
+    fig, ax = plt.subplots(figsize=(7, 7))
+    print("Made a figure")
+
+    # Set grid size
+    plt.xlim([statespace_lo[0], statespace_hi[0]])
+    plt.ylim([statespace_lo[1], statespace_hi[1]])
+    print("Set limits")
+
+    # Add obstacles as circular patches
+    for obs in obstacles:
+        centerX, centerY, radius = obs
+        circle = plt.Circle((centerX, centerY), radius, color='blue')
+        ax.add_patch(circle)
+        print("Added a circle")
+
+    # Plot each point on the path as a single point
+    plt.scatter(path[:,0], path[:,1], c='red')
+    print("Added path")
+
+    # Show and save (invert axes to be at top left)
+    plt.gca().invert_yaxis()
+    plt.savefig("vis.png")
+    plt.show()
+    print("Saved figure")
+
+
+def main():
+    # 1. get the grid, obstacles, and path from the files
+    grid = get_grid_from_file("grid.txt")
+    obstacles = get_obstacles_from_file("obstacles.txt")
+    path = get_path_from_file("rrt_path.txt")
+
+    # 2. plot everything
+    plot_everything(grid, obstacles, path)
+
+main()
--- test/rrt/rrt_test.cpp
+++ test/rrt/rrt_test.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "../include/MobilePlanner.hpp"
 #include "../include/OccupancyGrid.hpp"
 
 using namespace Sai2Planning;
 using namespace std;
 
-const string path_file = "./planned_path.txt";
+const string obs_file = "../../pytest/obstacles.txt";
+const string grid_file = "../../pytest/grid.txt";
+const string path_file = "../../pytest/rrt_path.txt";
 
 int main() {
 
-  // 1. Make a list of obstacles
+  // 1. Make a list of obstacles and write to file
   std::list<Obstacle> obstacles;
   Obstacle obsA = Obstacle(2.5, 10.0, 2.5); //Obstacle(0, 7, 5, 13);
   Obstacle obsB = Obstacle(5.0, 3.5, 3.0); //Obstacle(3, 2, 7, 5);
@@ -19,8 +22,26 @@ int main() {
   obstacles.push_back(obsB);
   obstacles.push_back(obsC);
 
-  // 2. Initialize an occupancy grid
+  ofstream obsfile(obs_file);
+  if(obsfile.is_open()) {
+    for (const Obstacle &obstacle : obstacles) {
+      obsfile << obstacle._centerX << "," << obstacle._centerY << "," << obstacle._radius << "\n";
+    }
+    obsfile.close();
+  } else {
+    std::cerr<<"Unable to open file";
+  }
+
+  // 2. Initialize an occupancy grid and write to file
   OccupancyGrid grid = OccupancyGrid(14, 14, obstacles, 1.0);
+
+  ofstream gridfile(grid_file);
+  if(gridfile.is_open()) {
+    gridfile << grid._width << "," << grid._height << "," << grid._resolution << "\n";
+    gridfile.close();
+  } else {
+    std::cerr<<"Unable to open file";
+  }
 
   // 3. Initialize a mobile planner
   Eigen::VectorXd statespace_lo(2);
@@ -43,8 +64,15 @@ int main() {
   Eigen::MatrixXd smoothed = planner.computeSmoothedTrajectory(path, V_des, alpha, dt);
 
   // 5. Go through rows and save positions to a txt file
-  for (int i = 0; i < smoothed.rows(); i++) {
-    std::cout << smoothed.row(i) << "\n";
+  ofstream myfile("../../pytest/rrt_path.txt");
+  if(myfile.is_open()) {
+    for (int i = 0; i < smoothed.rows(); i++) {
+      std::cout << smoothed.row(i)(0) << "," << smoothed.row(i)(1) << "\n";
+      myfile << smoothed.row(i)(0) << "," << smoothed.row(i)(1) << "\n";
+    }
+    myfile.close();
+  } else {
+    std::cerr<<"Unable to open file";
   }
 
   // 6. Plot the map and the path in python
